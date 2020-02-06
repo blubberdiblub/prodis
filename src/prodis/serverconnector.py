d51bee45804a7c31b8ddd0360f4c54ed6a4e94d3
Niels Boehm <blubberdiblub@gmail.com>
2020-02-06 19:03:49 +0100

    Let ServerConnector reconnect

--- src/prodis/serverconnector.py
+++ src/prodis/serverconnector.py
@@ -13,19 +13,32 @@ _log = _Logger(__name__)
 
 class ServerConnector(_ForeverTask):
 
-    def __init__(self, host: str = 'localhost', port: int = 14454) -> None:
+    def __init__(self, host: str = 'localhost', port: int = 14454,
+                 retry_delay: int = 3, reconnect_delay: int = 1) -> None:
 
         super().__init__()
 
         self.host = host
         self.port = port
+        self.retry_delay = retry_delay
+        self.reconnect_delay = reconnect_delay
 
     async def _coroutine(self) -> None:
 
-        reader, writer = await _asyncio.open_connection(
-            host=self.host,
-            port=self.port,
-        )
+        while True:
+
+            try:
+                reader, writer = await _asyncio.open_connection(
+                    host=self.host,
+                    port=self.port,
+                )
+
+            except ConnectionError:
+
+                await _asyncio.sleep(self.retry_delay)
+                continue
+
+            await _asyncio.sleep(self.reconnect_delay)
 
     def _get_future(self, loop: _asyncio.AbstractEventLoop) -> _asyncio.Task:
 
